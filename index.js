f3ead689f25f4046090ff71b11982df42a7d47b4
Beanie Pies <kgravesjohnson@gmail.com>
2018-09-17 01:36:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget(uniqueID, classN) {
+  return document.querySelector(uniqueID).getElementById(classN);
+}
+
+function increaseRankBy(n) {
+  var lis = document.querySelectorAll("ul .ranked-list li");
+  for (var i = 0; i < lis.length; i++) {
+    lis.innerHTML[i] = (parseInt(lis.innerHTML[i]) + 1).toString();
+  }
+}
+
+function deepestChild() {
+  var lis = document.getElementById("grand-node");
+  var n = lis.length - 1;
+  return lis[n];
+  
+}
